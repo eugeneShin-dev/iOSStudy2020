b50a57842415536a5485c66a93159cd3ccecac07
hailey-hawaii <hailey.hawaii@kakao.com>
2020-10-28 11:45:19 +0900

    최초커밋

---
+++ Chapter07-CoreData/Chapter07-CoreData/ListViewController.swift
@@ -0,0 +1,175 @@
+//
+//  ListViewController.swift
+//  Chapter07-CoreData
+//
+//  Created by Hailey on 2020/10/27.
+//
+
+import UIKit
+import CoreData
+
+class ListViewController: UITableViewController {
+    lazy var list: [NSManagedObject] = {
+        return self.fetch()
+    }()
+    
+    func fetch() -> [NSManagedObject] {
+        // 1. 앱 델리게이트 참조
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        // 2. 관리 객체 컨텍스트 참조
+        let context = appDelegate.persistentContainer.viewContext
+        // 3. 요청 객체 생성: SELECT 문과 유사, 엔터티 이름을 인자로 줘서 어느 엔터티인지 결정
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Board")
+        
+        let sort = NSSortDescriptor(key: "regdate", ascending: false)
+        fetchRequest.sortDescriptors = [sort]
+        
+        // 4. 데이터 가져오기
+        let result = try! context.fetch(fetchRequest)
+        return result
+    }
+    
+    func save(title: String, contents: String) -> Bool {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        // 관리 객체 생성 & 값 설정
+        let object = NSEntityDescription.insertNewObject(forEntityName: "Board", into: context)
+        object.setValue(title, forKey: "title")
+        object.setValue(contents, forKey: "contents")
+        object.setValue(Date(), forKey: "regdate")
+        
+        // 커밋하기
+        do {
+            try context.save()
+//            self.list.append(object)
+            self.list.insert(object, at: 0)
+            return true
+        } catch {
+            context.rollback()
+            return false
+        }
+    }
+    
+    func delete(object: NSManagedObject) -> Bool {
+        let appDelegte = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegte.persistentContainer.viewContext
+        
+        context.delete(object)
+        
+        do {
+            try context.save().self
+            return true
+        } catch {
+            context.rollback()
+            return false
+        }
+    }
+    
+    func edit(object: NSManagedObject, title: String, contents: String) -> Bool {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        object.setValue(title, forKey: "title")
+        object.setValue(contents, forKey: "contents")
+        object.setValue(Date(), forKey: "regdate")
+        
+        do {
+            try context.save()
+            self.list = self.fetch()
+            return true
+        } catch {
+            context.rollback()
+            return false
+        }
+    }
+    
+    @objc func add(_ snder: Any) {
+        let alert = UIAlertController(title: "게시글 등록", message: nil, preferredStyle: .alert)
+        alert.addTextField { $0.placeholder = "제목" }
+        alert.addTextField { $0.placeholder = "내용" }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Save", style: .default) {(_) in
+            guard let title = alert.textFields?.first?.text,
+                  let contents = alert.textFields?.last?.text else {
+                return
+            }
+            
+            if self.save(title: title, contents: contents) == true {
+                self.tableView.reloadData()
+            }
+        })
+        self.present(alert, animated: false)
+    }
+    
+    override func viewDidLoad() {
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(add(_:)))
+        self.navigationItem.rightBarButtonItem = addButton
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let record = self.list[indexPath.row]
+        let title = record.value(forKey: "title") as? String
+        let contents = record.value(forKey: "contents") as? String
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = title
+        cell.detailTextLabel?.text = contents
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        let object = self.list[indexPath.row]
+        
+        if self.delete(object: object) {
+            self.list.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 데이터 가져오기
+        let object = self.list[indexPath.row]
+        let title = object.value(forKey: "title") as? String
+        let contents = object.value(forKey: "contents") as? String
+        
+        let alert = UIAlertController(title: "게시글 수정", message: nil, preferredStyle: .alert)
+        
+        // 입력 필드 추가
+        alert.addTextField() { $0.text = title }
+        alert.addTextField() { $0.text = contents }
+        
+        // 버튼 추가
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        alert.addAction(UIAlertAction(title: "Save", style: .default) { (_) in
+            guard let title = alert.textFields?.first?.text,
+                  let contents = alert.textFields?.last?.text else {
+                return
+            }
+            
+            // 값 수정하는 메소드 호출, 성공시 테이블 뷰 리로드
+            if self.edit(object: object, title: title, contents: contents) == true {
+//                self.tableView.reloadData()
+                
+                let cell = self.tableView.cellForRow(at: indexPath)
+                cell?.textLabel?.text = title
+                cell?.detailTextLabel?.text = contents
+                
+                let firstIndexPath = IndexPath(item: 0, section: 0)
+                self.tableView.moveRow(at: indexPath, to: firstIndexPath)
+            }
+        })
+        
+        self.present(alert, animated: false)
+    }
+}

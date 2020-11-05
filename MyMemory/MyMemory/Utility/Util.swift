0c99498534f3a0501b4cfdd34da7110788d75d99
hailey-hawaii <hailey.hawaii@kakao.com>
2020-11-05 15:18:34 +0900

    [재은씨8.4.2] 입력 필드 및 프로필 이미지 설정 기능 구현

--- MyMemory/MyMemory/Utility/Util.swift
+++ MyMemory/MyMemory/Utility/Util.swift
@@ -16,4 +16,15 @@ extension UIViewController {
     func instanceTutorialViewController(name: String) -> UIViewController {
         return self.tutorialStoryBoard.instantiateViewController(withIdentifier: name)
     }
+    
+    func alert(_ message: String, completion: (()->Void)? = nil) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "확인", style: .cancel) { (_) in
+                completion?()
+            }
+            alert.addAction(okAction)
+            self.present(alert, animated: false)
+        }
+    }
 }
